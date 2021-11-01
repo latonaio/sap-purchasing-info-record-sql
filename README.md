365247e13cbd3896adcbd8d6f0cbeb12f21a4935
MiyuYokoyama0109 <miyu.y@latona.io>
2021-11-01 15:23:12 +0900

    release

---
+++ README.md
@@ -0,0 +1,13 @@
+# sap-purchasing-info-record-sql
+sap-purchasing-info-record-sql は、主にエッジアプリケーションにおいて、SAPと連携された購買情報レコードを保存するSQLテーブルを作成するためのレポジトリです。
+
+
+## sqlの設定ファイル
+sap-purchasing-info-record-sql には、sqlの設定ファイルとして以下のsqlファイルが含まれています。  
+ 
+* sap-purchasing-info-record-sql.sql（SAP 購買情報レコード）
+* sap-purchasing-info-record-sql-pricing-condition.sq）（SAP購買情報レコード-価格条件データ）
+
+
+## MySQLのセットアップ / Kubernetesの設定 / SQLテーブルの作成方法
+MySQLのセットアップ / Kubernetesの設定 / 具体的なSQLテーブルの作成方法、については、[mysql-kube](https://github.com/latonaio/mysql-kube)を参照ください。
---
+++ sap-purchasing-info-record-sql-pricing-condition.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `sap-purchasing-info-record-pricing-condition`
+(
+    `PurchasingInfoRecord`  varchar(10) DEFAULT NULL,
+    `ConditionRecord`       varchar(10) DEFAULT NULL,
+    `ConditionValidityEndDate` date DEFAULT NULL,
+    `ConditionValidityStartDate` date DEFAULT NULL,
+    `ConditionSequentialNumber` int(2) DEFAULT NULL,
+    `ConditionApplication`  varchar(2) DEFAULT NULL,
+    `ConditionType`         varchar(4) DEFAULT NULL,
+    `ConditionScaleQuantity` float(13) DEFAULT NULL,
+    `ConditionScaleQuantityUnit` varchar(3) DEFAULT NULL,
+    `ConditionScaleAmount`  float(11) DEFAULT NULL,
+    `ConditionRateValue`    float(11) DEFAULT NULL,
+    `ConditionQuantity`     float(11) DEFAULT NULL,
+    PRIMARY KEY (`PurchasingInfoRecord`),
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;

9f1ff02336a959b93b2ce17cb6ba608ab61583cf
Yurino Oga <yurino.o@latona.io>
2021-11-02 15:15:54 +0900

    release

--- sap-purchasing-info-record-sql-pricing-condition.sql
+++ sap-purchasing-info-record-sql-pricing-condition.sql
@@ -12,6 +12,7 @@ CREATE TABLE `sap-purchasing-info-record-pricing-condition`
     `ConditionScaleAmount`  float(11) DEFAULT NULL,
     `ConditionRateValue`    float(11) DEFAULT NULL,
     `ConditionQuantity`     float(11) DEFAULT NULL,
-    PRIMARY KEY (`PurchasingInfoRecord`),
+    PRIMARY KEY (`PurchasingInfoRecord`, `ConditionRecord`, `ConditionSequentialNumber`),
+    CONSTRAINT `PurchasingInfoRecord_fk` FOREIGN KEY (`PurchasingInfoRecord`) REFERENCES `sap-purchasing-info-record` (`PurchasingInfoRecord`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
